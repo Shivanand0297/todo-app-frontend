0dccc49eae6b27d76d954514c8dfb118db40df41
alok verma <alokverma749@gmail.com>
2023-01-20 02:34:25 +0530

    fix routing and user state context

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -1,2 +1,15 @@
-import { createContext } from "react";
+import { createContext, useState,useEffect } from "react";
 export const UserContext = createContext()
+
+export const UserContextProvider = ({ children }) => {
+    const [user, setUser] = useState(null)
+    useEffect(() => {
+        const token = JSON.parse(localStorage.getItem('user'))
+        setUser(token)
+    }, [])
+    return (
+        <UserContext.Provider value={{ user, setUser }}>
+            {children}
+        </UserContext.Provider>
+    )
+}
